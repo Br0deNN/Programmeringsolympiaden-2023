187f3f4598b4cbd1f91d50acfc12cf705f961ee4
7R1XY <143171992+Br0deNN@users.noreply.github.com>
2023-11-24 11:43:48 +0100

    Add files via upload

---
+++ Uppgift 3 - Meddelande/Uppgift 3 - Meddelande/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace uppgift_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            Console.WriteLine("n ?");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("m ?");
+            int y = int.Parse(Console.ReadLine());
+
+            
+            for (int i = 0; i < x; i++)
+            {
+               Console.WriteLine($"Rad {i+1} ?");
+                string z = Console.ReadLine();
+                string[] a = z.Split('.');
+
+                //inte klar
+
+                
+
+
+            }
+
+            
+           
+
+            
+        }
+    }
+}
